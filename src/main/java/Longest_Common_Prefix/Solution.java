bf1bf8b5d32e7e77683abf485d84f2993c6842b2
Sergei <svnat92@gmail.com>
2022-09-28 10:49:03 +0300

    Решил Longest Common Prefix

--- src/main/java/Longest_Common_Prefix/Solution.java
+++ src/main/java/Longest_Common_Prefix/Solution.java
@@ -4,5 +4,30 @@ public class Solution {
 
     public String longestCommonPrefix(String[] strs) {
 
+        StringBuilder prefix = new StringBuilder();
+
+        int maxPrefixLength = 200;
+
+        if (strs.length == 1) {
+            return strs[0];
+        }
+
+        try {
+            for (int i = 0; i < maxPrefixLength; i++) {
+                Character previous = null;
+                for (String str :
+                        strs) {
+                    Character current = str.charAt(i);
+                    if (current != previous && previous != null) {
+                        return prefix.toString();
+                    }
+                    previous = current;
+                }
+                prefix.append(previous);
+            }
+            return prefix.toString();
+        } catch (IndexOutOfBoundsException | NullPointerException e) {
+            return prefix.toString();
+        }
     }
 }
