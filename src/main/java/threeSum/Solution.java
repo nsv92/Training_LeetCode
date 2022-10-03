dec21477f43d56cb9668230a28904994772bda53
Sergei <svnat92@gmail.com>
2022-10-04 01:30:53 +0300

    3Sum
    Неудачная попытка

--- src/main/java/threeSum/Solution.java
+++ src/main/java/threeSum/Solution.java
@@ -1,10 +1,31 @@
 package threeSum;
 
-import java.util.List;
+import java.util.*;
 
 class Solution {
 
     public List<List<Integer>> threeSum(int[] nums) {
+
+        int[] sorted = Arrays.stream(nums).sorted().toArray();
+
+
+
+        int firstCurrentIndex;
+
+        int secondCurrentIndex;
+
+        int key = 0 - firstCurrentIndex - secondCurrentIndex;
+
+        if (firstCurrentIndex == 0) {
+            Arrays.binarySearch(sorted, 1, sorted.length, )
+
+        }
+
+        Arrays.binarySearch()
+
+        Set<int[]> resultSet = new HashSet<>();
+
+
 
         return null;
     }
